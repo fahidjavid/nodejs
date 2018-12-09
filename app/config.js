33ad1983ffe570c7149f2479c9138a1e71b7646e
Fahid Javid <fahidjavid@gmail.com>
2018-12-09 21:47:34 +0500

    Completed the APP config file.

--- app/config.js
+++ app/config.js
@@ -2,3 +2,27 @@
 * Create and export configuration variables
 *
 */
+
+
+// Container for  all the environments
+enviroments = {};
+
+// Staging (default) environment
+enviroments.staging = {
+  'port' : 3000,
+  'envName' : 'staging'
+};
+
+enviroments.production = {
+    'port' : 5000,
+    'envName' : 'production'
+};
+
+// Determine which environment was passed as a command-line argument
+var currentEvnvironment = typeof(process.evn.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check that the current environment is one of the environments above, if not, default to staging
+var environmentToExport = typeof(environments[currentEvnvironment]) == 'object' ? enviroments[currentEvnvironment] : enviroments.staging;
+
+// Export the module
+module.exports = environmentToExport;
